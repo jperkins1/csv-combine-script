7a94ed7dc88e7f203532d0408bbb46140c3e9179
John Perkins <59887992+jperkins1@users.noreply.github.com>
2020-10-03 11:48:30 -0500

    Update csv-combine-script.sh

--- csv-combine-script.sh
+++ csv-combine-script.sh
@@ -4,4 +4,4 @@
 read -p "Where are your images stored? " directory
 
 # Executing the Gawk script (also cloned from Github)
-awk -f script.awk $directory/* > $directory/output.csv
+awk -f script.awk $directory/*.csv > $directory/output.csv
