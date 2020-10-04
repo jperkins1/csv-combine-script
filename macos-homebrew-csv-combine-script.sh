d46d05a83156ccc64b4250f8ed8f45e99c14ed7a
John Perkins <59887992+jperkins1@users.noreply.github.com>
2020-10-04 16:30:14 -0500

    Update macos-homebrew-csv-combine-script.sh

--- macos-homebrew-csv-combine-script.sh
+++ macos-homebrew-csv-combine-script.sh
@@ -15,5 +15,6 @@ brew install gawk
 # Asking for user input on where the CSV files are stored
 read -p "Where are your images stored? " directory
 
-# Executing the Gawk script (also cloned from Github)
-awk -f script.awk $directory/*.csv > $directory/output.csv
+# Creating the output.csv file with the filenames and executing the Gawk script (also cloned from Github) to put the 3rd column into the file under each respective filename.
+awk -F, 'FNR == 1{ print FILENAME}' ORS=',' $directory/*.csv > $directory/output.csv
+awk -f script.awk $directory/*.csv >> $directory/output.csv
