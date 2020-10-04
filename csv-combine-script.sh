1b74bfb4ea4ef4288f5dd034ad024738c6e0b7ca
John Perkins <59887992+jperkins1@users.noreply.github.com>
2020-10-04 17:16:04 -0500

    Update csv-combine-script.sh

--- csv-combine-script.sh
+++ csv-combine-script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Asking for user input on where the CSV files are stored
-read -p "Where are your images stored? " directory
+read -p "Where are your files stored? (the Lab NAS is mounted at /Volumes/Tyska\ Lab\ NAS/) " directory
 
 # Creating the output.csv file with the filenames and executing the Gawk script (also cloned from Github) to put the 3rd column into the file under each respective filename.
-awk -F, 'FNR == 1{ print FILENAME}' ORS=',' $directory/* > $directory/output.csv
-awk -f /Users/$(whoami)/Desktop/csv-combine-script/script.awk $directory/* >> $directory/output.csv
+awk -F, 'FNR == 1{ print FILENAME}' ORS=',' $directory/*.txt > $directory/output.csv
+awk -f /Users/$(whoami)/Desktop/csv-combine-script/script.awk $directory/*.txt >> $directory/output.csv
