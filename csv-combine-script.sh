dfe4e21ef27d72b437168a270e97a5a7573cac53
John Perkins <59887992+jperkins1@users.noreply.github.com>
2020-10-06 14:22:22 -0500

    Update csv-combine-script.sh

--- csv-combine-script.sh
+++ csv-combine-script.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# Asking for user input on where the CSV files are stored
 read -p "Where are your files stored? (drag and drop from Finder) " directory
 
-# Creating the output.csv file with the filenames and executing the Gawk script (also cloned from Github) to put the 3rd column into the file under each respective filename.
-awk -F, 'FNR == 1{ print FILENAME,"\n"}' ORS=',' $directory/*.txt > $directory/output.csv
-awk -f /Users/$(whoami)/Desktop/csv-combine-script/script.awk $directory/*.txt >> $directory/output.csv
+# Creating .csv files from your .txt files. The original .txt files will not be altered. 
+echo "Creating .csv files..."
+
+for n in $directory/*.txt
+do 
+	sed 's/\s\+/,/g' "${n}" > "${n}".csv
+done
+
+# Executing the script.awk file to take the filename and 3rd column of each of the previously created .csv files and put those all into one output file. 
+echo "Creating the output file in the directory you specified."
+awk -f script.awk $directory/*.csv > $directory/output.csv
